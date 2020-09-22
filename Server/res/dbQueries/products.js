4f49b588600569263b9d508784afb22f2e17404e
smob123 <smob123@hotmail.com>
2020-09-22 18:28:27 +1200

    connected to mongodb and implemented retrieving data from it

---
+++ Server/res/dbQueries/products.js
@@ -0,0 +1,61 @@
+/**
+ * responsible for making database queries te fetch products.
+ */
+
+const Product = require('../models/productModle');
+
+class ProductQueries {
+    async getRandomProducts(size) {
+        let products = await Product.find();
+        this.shuffle(products);
+        if (size && size > -1) {
+            return products.slice(0, size);
+        } else {
+            return products;
+        }
+    }
+
+    /**
+     * shuffles an array.
+     * 
+     * @param arr the array of elements that we want to shuffle
+     */
+    shuffle(arr) {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const index = Math.floor(Math.random() * i);
+            const temp = arr[index];
+            arr[index] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    async getAllTShirts() {
+        return await Product.find({ productType: 'T-shirts' });
+    }
+
+    async getAllPants() {
+        return await Product.find({ productType: 'Pants' });
+    }
+
+    async getAllHoodies() {
+        return await Product.find({ productType: 'Hoodies' });
+    }
+
+    async getAllSuits() {
+        return await Product.find({ productType: 'Suits & Blazers' });
+    }
+
+    async getAllUnderwear() {
+        return await Product.find({ productType: 'Underwear' });
+    }
+
+    async getAllSocks() {
+        return await Product.find({ productType: 'Socks' });
+    }
+
+    async getAllAccessories() {
+        return await Product.find({ productType: 'Accessories' });
+    }
+}
+
+module.exports = new ProductQueries();
--- Server/server.js
+++ Server/server.js
@@ -1,24 +1,60 @@
 const express = require('express');
 const app = express();
 
-/*TODO: connect to MongoDB and pull information needed for each route 
-e.g. when /products is requested pull from the database info about all 
-products and return that as a response.
+// allows cross origin resource sharing
+const cors = require('cors');
+app.use(cors());
 
+// configure database connection
+const mongoose = require('mongoose');
+const dbUrl = require('./res/config/dbConfig');
 
-*/
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+// make sure that the connection has been established
+mongoose.connection.once(('open'), async () => {
+  console.log('connected to the database!');
+});
+
+const ProductQueries = require('./res/dbQueries/products');
+
 app.get('/', (req, res) => {
   const test = [
     { id: 1, name: 'meme' },
-    {id: 3, name: 'second thing '}
+    { id: 3, name: 'second thing ' }
   ]
   res.json(test);
 })
-app.get('/products', (req, res) => {
-  const test = [
-    { id: 3, name: 'hello' }
-  ]
-  res.json(test);
+app.get('/products', async (req, res) => {
+  const products = await ProductQueries.getRandomProducts(10);
+  res.json(products);
+})
+app.get('/tshirts', async (req, res) => {
+  const tshirts = await ProductQueries.getAllTShirts();
+  res.json({ tshirts });
+})
+app.get('/pants', async (req, res) => {
+  const pants = await ProductQueries.getAllPants();
+  res.json({ pants });
+})
+app.get('/hoodies', async (req, res) => {
+  const hoodies = await ProductQueries.getAllHoodies();
+  res.json({ hoodies });
+})
+app.get('/suits', async (req, res) => {
+  const suits = await ProductQueries.getAllSuits();
+  res.json({ suits });
+})
+app.get('/underwear', async (req, res) => {
+  const underwear = await ProductQueries.getAllUnderwear();
+  res.json({ underwear });
+})
+app.get('/socks', async (req, res) => {
+  const socks = await ProductQueries.getAllSocks();
+  res.json({ socks });
+})
+app.get('/accessories', async (req, res) => {
+  const accessories = await ProductQueries.getAllAccessories();
+  res.json({ accessories });
 })
 app.get('/cart', (req, res) => {
   const test = [
