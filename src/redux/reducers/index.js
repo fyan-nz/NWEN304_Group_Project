9461c41fe8740dbe34dffa77fb8144c5f5c819df
Fangyi Yan <yanfang@myvuw.ac.nz>
2020-10-07 22:19:10 +1300

    move folders

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * combines reducers into a single root reducer
- */
-import CartReducer from './cartReducer';
-import { combineReducers } from 'redux';
-
-export default combineReducers({
-    cart: CartReducer
-});
