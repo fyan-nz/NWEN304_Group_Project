adbba3f78680e875332f17d365bb851b0dd5251d
Fangyi Yan <yanfangyi@gmail.com>
2020-10-19 23:14:45 +1300

    minor fix

--- web application/res/routes/api/index.js
+++ web application/res/routes/api/index.js
@@ -28,54 +28,54 @@ router.get('/:productType', async (req, res) => {
 //POST Route for registration
 router.post('/register', async (req, res) => {
     if (!req.body.email) {
-        res.status(400).json({ message: 'email paramter is missing' })
+        res.status(400).json({message: 'email paramter is missing'})
         return;
     }
 
     if (!req.body.password) {
-        res.status(400).json({ message: 'password paramter is missing' })
+        res.status(400).json({message: 'password paramter is missing'})
         return;
     }
 
     try {
         const result = await UserQueries.register(req.body.email, req.body.password);
         req.session.user = result;
-        res.status(200).json({ message: 'registration successful' });
+        res.status(200).json({message: 'registration successful'});
     } catch (err) {
         if (err.message.includes("Email address exist")) {
-            res.status(401).json({ message: "Email address exist" });
+            res.status(401).json({message: "Email address exist"});
         } else if (err.message.includes('password is too weak')) {
-            res.status(401).json({ message: "password is too weak" });
+            res.status(401).json({message: "password is too weak"});
         } else {
-            res.status(400).json({ message: "something went wrong" });
+            res.status(400).json({message: "something went wrong"});
         }
     }
 });
 
 //POST route for Login
 router.post('/login', async (req, res) => {
-
+    console.log("body: ",req.body)
+    console.log("query: ",req.query)
     if (!req.body.email) {
-        res.status(400).json({ message: 'email paramter is missing' })
+        res.status(400).json({message: 'email parameter is missing'})
         return;
     }
-
     if (!req.body.password) {
-        res.status(400).json({ message: 'password paramter is missing' })
+        res.status(400).json({message: 'password parameter is missing'})
         return;
     }
 
     try {
         const result = await UserQueries.login(req.body.email, req.body.password);
         req.session.user = result;
-        res.status(200).json({ message: "login successful" });
+        res.status(200).json({message: "login successful"});
     } catch (err) {
         if (err.message.includes("password doesn't match")) {
-            res.status(401).json({ message: "password doesn't match" });
+            res.status(401).json({message: "password doesn't match"});
         } else if (err.message.includes("no account exists with the given credentials")) {
-            res.status(400).json({ message: "no account exists with the given credentials" });
+            res.status(400).json({message: "no account exists with the given credentials"});
         } else {
-            res.status(400).json({ message: "something went wrong" });
+            res.status(400).json({message: "something went wrong"});
         }
     }
 })
